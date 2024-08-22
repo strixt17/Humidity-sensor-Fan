5e618e745ff2d0d8439dba4f2f4b75c3ac0ebbbf
strixt17 <nathanvarghese09@gmail.com>
2024-08-22 08:14:08 -0400

    3rd hour commit

--- Humidity_Sensor.ino
+++ Humidity_Sensor.ino
@@ -7,6 +7,7 @@ int motorPin = 3;
 int redPin = 4;
 int bluePin = 5;
 int greenPin = 6;
+int buzzerPin = 2;
 
 void setup()
 {  
@@ -19,25 +20,33 @@ void setup()
 
 void loop()
 {
-  tempInput = analogRead(A0); 
-  temp = (double)tempInput / 1024; 
-  temp = (temp * 5 - 0.5) * 100; 
-  Serial.print("Current Temperature: ");
-  Serial.println(temp);
-    
+	tempInput = analogRead(A0); 
+	temp = (double)tempInput / 1024; 
+	temp = (temp * 5 - 0.5) * 100; 
+	Serial.print("Current Temperature: ");
+	Serial.println(temp);
+  	
   if (temp <= 0) {
-    digitalWrite(redPin, 0);
+  	digitalWrite(redPin, 0);
     digitalWrite(greenPin, 0);
     digitalWrite(bluePin, 252);
+    digitalWrite(motorPin, LOW);
+    noTone(buzzerPin);
   }
   if (temp >= 0 && temp <= 45) {
-    digitalWrite(redPin, 0);
+  	digitalWrite(redPin, 0);
     digitalWrite(greenPin, 252);
     digitalWrite(bluePin, 0);
+	noTone(buzzerPin);
   }
-  else {
-    digitalWrite(redPin, 252);
+  if (temp > 45) {
+  	digitalWrite(redPin, 252);
     digitalWrite(greenPin, 0);
     digitalWrite(bluePin, 0);
+    digitalWrite(motorPin, LOW);
+    tone(buzzerPin, 1000);
+    delay(1000);
+    noTone(buzzerPin);
+    delay(1000);
   }
 }
